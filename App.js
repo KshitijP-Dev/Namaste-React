232a4f2bed6982a19919befe87861eaaf3777d98
Patil <kshitij.ashok.patil@accenture.com>
2026-03-01 18:46:10 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1" ,{id : "heading"} ,"Namaste React from React.js!");
+const roor = ReactDOM.createRoot(document.getElementById("root"));
+roor.render(heading);
