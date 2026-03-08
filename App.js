393a9929a1faadc61ae24bfeadf86b8414ee5f99
KshitijP-Dev <kshitijkale1998@gmail.com>
2026-03-08 21:01:38 +0530

    episode-01

--- App.js
+++ App.js
@@ -1,3 +1,14 @@
-const heading = React.createElement("h1" ,{id : "heading"} ,"Namaste React from React.js!");
+const heading = React.createElement("h1" ,{id : "parent"},[
+    React.createElement("h1" ,{id : "child1"},
+        [React.createElement("h1" ,{},"I am an H1 tag!"),
+         React.createElement("h2" ,{},"I am an H2 tag!"),
+        ]),
+        React.createElement("h1" ,{id : "child2"},[
+            React.createElement("h1" ,{},"I am an H1 tag!"),
+         React.createElement("h2" ,{},"I am an H2 tag!"),
+        ]),
+    ]);   
+
+    // console.log(parent);
 const roor = ReactDOM.createRoot(document.getElementById("root"));
 roor.render(heading);
